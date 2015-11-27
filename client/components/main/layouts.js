a16efdad4bbbbee164309cd7da49e4e4adbede79
Liming Xie <liming.xie@gmail.com>
2015-11-27 22:59:29 +0800

    add: allow select lang for user form

--- client/components/main/layouts.js
+++ client/components/main/layouts.js
@@ -2,8 +2,50 @@ Meteor.subscribe('boards');
 
 BlazeLayout.setRoot('body');
 
+// see: https://github.com/softwarerero/meteor-accounts-t9n#language-codes-and-contributions
+const mapTagi18nToT9n = {
+  'es-ES': 'es_ES',
+  'no-NB': 'no_NB',
+  'pt-BR': 'pt',
+  'pt-PT': 'pt_PT',
+  'zh-CN': 'zh_cn',
+  'zh-HK': 'zh_hk',
+  'zh-TW': 'zh_tw',
+};
+
+const i18nTagToT9n = (i18nTag) => {
+  return mapTagi18nToT9n[i18nTag] || i18nTag.split('-')[0];
+};
+
 Template.userFormsLayout.onRendered(() => {
+  const i18nTag = navigator.language;
+  if (i18nTag) {
+    T9n.setLanguage(i18nTagToT9n(i18nTag));
+  }
   EscapeActions.executeAll();
+});
+
+Template.userFormsLayout.helpers({
+  languages() {
+    return _.map(TAPi18n.getLanguages(), (lang, tag) => {
+      const name = lang.name;
+      return { tag, name };
+    });
+  },
+
+  isCurrentLanguage() {
+    const t9nTag = i18nTagToT9n(this.tag);
+    const curLang = T9n.getLanguage() || 'en';
+    return t9nTag === curLang;
+  },
+});
+
+Template.userFormsLayout.events({
+  'change .js-userform-set-language'(evt) {
+    const i18nTag = $(evt.currentTarget).val();
+    T9n.setLanguage(i18nTagToT9n(i18nTag));
+    evt.preventDefault();
+  },
 });
 
 Template.defaultLayout.events({
